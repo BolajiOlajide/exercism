bd784803f4e1d7c6b0fe9dd0b3965085f68c897c
Bolaji Olajide <cooproton@gmail.com>
2019-10-01 21:28:20 +0100

    complete javascript resistor-color

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,20 @@
+//
+// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const colorCode = (color) => colorObject[color]
+
+const colorObject = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9
+};
+export const COLORS = Object.keys(colorObject);
