f99e1314454f772e232993d036bb04580da56ba7
Bolaji Olajide <cooproton@gmail.com>
2020-06-30 00:30:11 +0100

    implement feedback from telesphinx

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -6,12 +6,12 @@ import "errors"
 func Distance(a, b string) (int, error) {
 	var firstStringLength = len(a)
 	var secondStringLength = len(b)
-	var incorrectGenes = 0
 
 	if firstStringLength != secondStringLength {
-		return 0, errors.New("String length are not equal")
+		return 0, errors.New("string length are not equal")
 	}
 
+	var incorrectGenes = 0
 	for i := 0; i < firstStringLength; i++ {
 		if a[i] != b[i] {
 			incorrectGenes++
