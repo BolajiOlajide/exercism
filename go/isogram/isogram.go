049db2127a927d15ce752ddbe4c11657af36fddb
Bolaji Olajide <cooproton@gmail.com>
2020-12-22 18:28:32 +0100

    aasarrgggh! another isogram refactor

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -7,19 +7,19 @@ import (
 
 // IsIsogram returns a boolean to determine if the argument is an isogram
 func IsIsogram(input string) bool {
-	var tempMap = make(map[rune]int)
+	var tempMap = make(map[rune]bool)
 
 	for _, char := range strings.ToLower(input) {
 		if !unicode.IsLetter(char) {
 			continue
 		}
 
-		if tempMap[char] == 0 {
-			tempMap[char] = 1
-			continue
+		if tempMap[char] {
+			return false
 		}
 
-		return false
+		tempMap[char] = true
+		continue
 	}
 
 	return true
