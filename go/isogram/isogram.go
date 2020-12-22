68d049e8e845c61b0ac5d375d788a635e66b6cc4
Bolaji Olajide <cooproton@gmail.com>
2020-12-22 16:47:17 +0100

    refactor isogram implementation again

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,24 +1,24 @@
 package isogram
 
 import (
-	"regexp"
 	"strings"
+	"unicode"
 )
-
-// bolaji
-var invalidCharRegex *regexp.Regexp = regexp.MustCompile(`[- ]`)
 
 // IsIsogram returns a boolean to determine if the argument is an isogram
 func IsIsogram(input string) bool {
-	sanitizedInput := invalidCharRegex.ReplaceAllString(strings.ToLower(input), "")
-
 	var tempMap = make(map[rune]int)
 
-	for _, char := range sanitizedInput {
+	for _, char := range strings.ToLower(input) {
+		if !unicode.IsLetter(char) {
+			continue
+		}
+
 		if tempMap[char] == 0 {
 			tempMap[char] = 1
 			continue
 		}
+
 		return false
 	}
 
