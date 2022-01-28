1f14a1e9afb399671b281083a07a3cd314b49696
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2022-01-28 10:31:16 +0100

    (go) complete golang lasagna exercise

---
+++ go/lasagna/lasagna.go
@@ -0,0 +1,20 @@
+package lasagna
+
+// OvenTime indicates how many minutes the lasagna should be in the oven
+const OvenTime = 40
+
+// RemainingOvenTime returns the remaining minutes based on the `actual` minutes already in the oven.
+func RemainingOvenTime(actualMinutesInOven int) int {
+	return OvenTime - actualMinutesInOven
+}
+
+// PreparationTime calculates the time needed to prepare the lasagna based on the amount of layers.
+func PreparationTime(numberOfLayers int) int {
+	return 2 * numberOfLayers
+}
+
+// ElapsedTime calculates the total time needed to create and bake a lasagna.
+func ElapsedTime(numberOfLayers, actualMinutesInOven int) int {
+	prepTime := PreparationTime(numberOfLayers)
+	return actualMinutesInOven + prepTime
+}
